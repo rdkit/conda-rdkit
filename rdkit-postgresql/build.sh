d6c0dfa32f8d9ba6fa6b3077bd7208cd13ecc862
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-10-28 22:18:38 +0100

    configure the test postgresql installation to preload the rdkit extension

--- rdkit-postgresql/build.sh
+++ rdkit-postgresql/build.sh
@@ -9,6 +9,10 @@ export PGPORT=54321
 export PGDATA=$SRC_DIR/pgdata
 
 pg_ctl initdb
+
+# ensure that the rdkit extension is loaded at process startup
+echo "shared_preload_libraries = 'rdkit'" >> $PGDATA/postgresql.conf
+
 pg_ctl start -l $PGDATA/log.txt
 
 # wait a few seconds just to make sure that the server has started
