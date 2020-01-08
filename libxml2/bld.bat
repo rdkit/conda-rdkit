d3a5f88193c147b329a01604250712dcc93526c9
Greg Landrum <greg.landrum@gmail.com>
2020-01-08 17:10:30 +0100

    remove things we never build (#88)
    * remove things we never build
    
    * remove

--- libxml2/bld.bat
+++
@@ -1,8 +0,0 @@
-cd "%SRC_DIR%\win32"
-cscript configure.js compiler=msvc "prefix=%LIBRARY_PREFIX%" ^
-  "include=%LIBRARY_INC%" "lib=%LIBRARY_LIB%" ^
-  "incdir=%LIBRARY_INC%" "libdir=%LIBRARY_LIB%" ^
-  "sodir=%LIBRARY_BIN%" "bindir=%LIBRARY_BIN%" debug=no
-
-nmake /f Makefile.msvc
-nmake /f Makefile.msvc install
