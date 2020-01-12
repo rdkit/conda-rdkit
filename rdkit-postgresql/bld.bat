fd70a70f369af9aa2c797d00da6a22f27bcc8554
Paolo Tosco <paolo.tosco.mail@gmail.com>
2020-01-12 15:49:18 +0000

    Fixes broken PostgreSQL cartridge build on Windows (#86)
    * - fixes broken PostgreSQL cartridge build on Windows
    
    * fixes build failure connected to missing boost::iostreams

--- rdkit-postgresql/bld.bat
+++ rdkit-postgresql/bld.bat
@@ -1,6 +1,35 @@
 %PYTHON% "%RECIPE_DIR%\pkg_version.py"
 
-cd "%SRC_DIR%\Code\PgSQL\rdkit"
+cd "%SRC_DIR%"
+
+rmdir /s /q build 2> NUL
+mkdir build
+cd build
+:: in case there are any old psql builds: remove them
+rmdir /s /q Code\PgSQL 2> NUL
+
+cmake ^
+    -G "NMake Makefiles" ^
+    -D RDK_BUILD_PGSQL=ON ^
+    -D RDK_PGSQL_STATIC=ON ^
+    -D RDK_INSTALL_INTREE=OFF ^
+    -D RDK_INSTALL_STATIC_LIBS=OFF ^
+    -D RDK_INSTALL_DEV_COMPONENT=OFF ^
+    -D RDK_BUILD_INCHI_SUPPORT=ON ^
+    -D RDK_BUILD_AVALON_SUPPORT=ON ^
+    -D RDK_BUILD_FREESASA_SUPPORT=OFF ^
+    -D RDK_USE_FLEXBISON=OFF ^
+    -D RDK_BUILD_THREADSAFE_SSS=ON ^
+    -D RDK_TEST_MULTITHREADED=ON ^
+    -D RDK_OPTIMIZE_POPCNT=ON ^
+    -D RDK_BUILD_CPP_TESTS=OFF ^
+    -D RDK_BUILD_PYTHON_WRAPPERS=OFF ^
+    -D RDK_USE_BOOST_SERIALIZATION=OFF ^
+    -D CMAKE_SYSTEM_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D BOOST_ROOT=%PREFIX% -D Boost_NO_SYSTEM_PATHS=ON ^
+    -D CMAKE_BUILD_TYPE=Release ^
+    ..
 
 where jom 2> NUL
 if %ERRORLEVEL% equ 0 (
@@ -9,19 +38,9 @@ if %ERRORLEVEL% equ 0 (
     set MAKE_CMD=nmake
 )
 
-cmake ^
-    -G "NMake Makefiles" ^
-    -D CMAKE_SYSTEM_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIXX% ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D RDK_OPTIMIZE_POPCNT=ON ^
-    -D RDK_BUILD_AVALON_SUPPORT=ON ^
-    -D RDK_BUILD_INCHI_SUPPORT=ON ^
-    -D RDKit_DIR=%LIBRARY_PREFIX%\lib ^
-    .
-
 %MAKE_CMD%
 
+cd Code\PgSQL\rdkit
 call pgsql_install.bat
 
 set PGPORT=54321
@@ -35,8 +54,9 @@ echo shared_preload_libraries = 'rdkit' >> %PGDATA%\postgresql.conf
 pg_ctl -D %PGDATA% -l %PGDATA%/log.txt start
 
 rem wait a few seconds just to make sure that the server has started
-timeout /t 2 /nobreak > NUL
+ping -n 5 127.0.0.1 > NUL
 
+set "RDBASE=%SRC_DIR%"
 ctest -V
 set check_result=%ERRORLEVEL%
 
